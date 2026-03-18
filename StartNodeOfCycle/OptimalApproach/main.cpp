8ea82f268bc8f7869b5f1d71cc18cc4091c8fe81
ankitkandwal-git <sharmaankit49103@gmail.com>
2026-03-18 19:17:04 +0530

    StartNodeCycle

---
+++ StartNodeOfCycle/OptimalApproach/main.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* arrayToLL(int arr[], int n){
+    Node* head = new Node(arr[0]);
+    Node* curr = head;
+    for(int i=1; i<n; i++){
+        curr->next = new Node(arr[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+
+void printLL(Node* head){
+    Node* curr = head;
+    while(curr != NULL){
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+Node* startNodeOfCycle(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast){
+            slow = head;
+            while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
+
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    Node* head = arrayToLL(arr, n);
+    head = startNodeOfCycle(head);
+    if(head == NULL){
+        cout<<"No cycle found"<<endl;
+    }
+    else{
+        cout<<"Start node of cycle is: "<<head->data<<endl;
+    }
+}
